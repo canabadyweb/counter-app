97f255f36c137354a8a4429a88f1280b3e79849a
SelvaCanabady P <canabadyweb@gmail.com>
2020-03-29 09:48:50 +0000

    Updates

---
+++ precache-manifest.664add8b8a1cb21ae8b42a2638fcc3dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1a9fb03eed5f2a17e4c88c768e36f9",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "8b31464b297e7f00b591",
+    "url": "/counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "036f6331447f768a56e9",
+    "url": "/counter-app/static/js/2.e4e4eecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.e4e4eecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b31464b297e7f00b591",
+    "url": "/counter-app/static/js/main.b6b8856b.chunk.js"
+  },
+  {
+    "revision": "ff674a87fad12ba53ff1",
+    "url": "/counter-app/static/js/runtime-main.c8445eb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counter-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
